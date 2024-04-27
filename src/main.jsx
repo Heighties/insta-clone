d6c6bd2e34cd876fb2486b4abb40f6cfb9b55524
Heighties <heighties@gmail.com>
2024-04-27 18:43:23 +0200

    Chakra SetUp

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,36 @@ import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 // https://v2.chakra-ui.com/docs/styled-system/color-mode
 import { extendTheme } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+import { BrowserRouter } from "react-router-dom";
+
+//change color dark mode
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode("gray.100", "#000")(props),
+      color: mode("gray.800", "whiteAlpha.900")(props),
+    },
+  }),
+};
+
+// 2. Add your color mode config
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+// 3. extend the theme
+const theme = extendTheme({ config, styles });
+
+export default theme;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
