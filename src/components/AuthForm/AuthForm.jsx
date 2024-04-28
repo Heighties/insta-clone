f426ecae4bbb129c21025af953dc9c95a5a1bd0b
Heighties <heighties@gmail.com>
2024-04-28 04:24:31 +0200

    + AuthForm

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,3 +1,13 @@
+import { Box, VStack, Image, Input } from "@chakra-ui/react";
+
 export const AuthForm = () => {
-  return <div>AuthForm</div>;
+  return (
+    <Box border={"1px solid gray"} borderRadius={4} padding={5}>
+      <VStack spacing={4}>
+        <Image src="/logo.png" h={24} cursor={"pointer"} alt="Instagram" />
+        <Input placeholder="Email" fontSize={14} type="email" />
+        <Input placeholder="Password" fontSize={14} type="password" />
+      </VStack>
+    </Box>
+  );
 };
