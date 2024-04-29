0f9a87d2e560c56af2b9dc762c83e8f5c3851c60
Heighties <heighties@gmail.com>
2024-04-29 19:14:25 +0200

    initialization PostHeader + PostFooter

---
+++ src/components/FeedPosts/PostHeader.jsx
@@ -0,0 +1,14 @@
+import { Avatar, Box, Flex } from "@chakra-ui/react"
+
+const PostHeader = () => {
+  return (
+    <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
+        <Flex alignItems={"center"} gap={2}>
+            <Avatar src="img1.png"/>
+        </Flex>
+        <Box>Pouet</Box>
+    </Flex>
+  )
+}
+
+export default PostHeader
