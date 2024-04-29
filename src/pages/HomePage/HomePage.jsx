6b97f8f519362c672fb60a43cd55b1226f407447
Heighties <heighties@gmail.com>
2024-04-29 13:20:22 +0200

    + Box FeedPost and Suggested Users in HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,16 @@
+import { Box, Container, Flex } from "@chakra-ui/react";
+
 export const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <Container maxW={"container.lg"}>
+      <Flex gap={20}>
+        <Box flex={2} py={10}>
+          FeedPosts
+        </Box>
+        <Box flex={3} mr={20} display={{base:"none", lg:"block"}} maxW={"300px"} border={"1px solid red"}>
+          Suggested Users
+        </Box>
+      </Flex>
+    </Container>
+  ) ;
 };
